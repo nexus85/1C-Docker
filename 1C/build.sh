a5da276d5caf42f798c2fb2c60453d006bfd9b69
nexus85 <mx38@mail.ru>
2017-12-04 16:43:33 +0300

    add volume license for

--- 1C/build.sh
+++ 1C/build.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-docker build --tag nexus85/1c-server:8.3.10-2466 .
+docker build --tag nexus85/1c-server:8.3.10-2466_2 .
--- 1C/run.sh
+++ 1C/run.sh
@@ -6,4 +6,4 @@ docker run --name 1c-server \
   --volume 1c-server-home:/home/usr1cv8 \
   --volume 1c-server-logs:/var/log/1C \
   --volume /etc/localtime:/etc/localtime:ro \
-  nexus85/1c-server
+  nexus85/1c-server:8.3.10-2466v2
