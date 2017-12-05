73e88a0dfb0e11759538781525bccd2b9c9c1fee
nexus85 <mx38@mail.ru>
2017-12-05 13:06:56 +0300

    add postgres.conf

--- PG/container/postgresql.conf.sh
+++ PG/container/postgresql.conf.sh
@@ -15,4 +15,34 @@ listen_addresses = '*'
 logging_collector = on
 log_line_prefix = '%m '
 
+
+autovacuum_max_workers = 10
+ssl = off
+shared_buffers = 5GB
+temp_buffers = 500MB
+work_mem = 8388kB
+maintenance_work_mem = 2GB
+effective_cache_size = 8GB
+effective_io_concurrency = 8
+random_page_cost = 1.0
+autovacuum = on
+autovacuum_naptime = 20s
+bgwriter_delay = 20ms
+bgwriter_lru_multiplier = 4.0
+bgwriter_lru_maxpages = 400
+synchronous_commit = off
+checkpoint_completion_target = 0.9
+max_wal_size = 8GB
+min_wal_size = 4GB
+fsync = on
+commit_delay = 50
+
+
+max_locks_per_transaction = 150
+max_connections = 500
+standard_conforming_strings = off
+escape_string_warning = off
+
+
+
 EOF
